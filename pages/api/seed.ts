7c41cb436da30dc2793bc1cf27d10f1a4f2097e7
Inyer Marín <81336145+InyerM@users.noreply.github.com>
2023-01-21 21:51:05 -0500

    UPDATE: seed endpoint

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,27 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { db, seedDb } from '../../database'
-import { Product, User } from '../../models'
+import type { NextApiRequest, NextApiResponse } from "next"
+import { db, seedDb } from "../../database"
+import { Order, Product, User } from "../../models"
 
 type Data = {
   message: string
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-
-  if(process.env.NODE_ENV === 'production') return res.status(401).json({ message: 'Not allowed' })
+  if (process.env.NODE_ENV === "production") return res.status(401).json({ message: "Not allowed" })
   try {
     await db.connect()
 
     await User.deleteMany()
-
     await User.insertMany(seedDb.initialData.users)
 
     await Product.deleteMany()
-
     await Product.insertMany(seedDb.initialData.products)
 
+    await Order.deleteMany()
+
     await db.disconnect()
-    res.status(200).json({ message: 'Successfully' })
+    res.status(200).json({ message: "Successfully" })
   } catch (error: any) {
     res.status(500).json({ message: error.message as string })
   }
